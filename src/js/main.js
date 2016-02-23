bb6fe12d95bd841a2fdc5f5dabb5962298b22f02
Web Superhero <superhero@itfactory.neta>
2016-02-23 19:36:52 +0100

    Copy all existing items.

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Userek lekérése.
+jQuery.getJSON('users', function (users) {
+    console.log('users', users);
+
+    var unDef = 1;
+
+    if (true) {
+        console.log(unDef);
+    }
+
+});
